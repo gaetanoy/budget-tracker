f66152507aac65d8c9302b6352c12663998401b0
Olivier <olivier.gabelle1@gmail.com>
2025-12-05 19:56:18 +0100

    remove unique constraint on category name

--- database/models/category.py
+++ database/models/category.py
@@ -7,7 +7,7 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("user.id"))
-    name = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, index=True, nullable=False)
     color = Column(String, nullable=True)
     icon = Column(String, nullable=True)
     user = relationship("User", back_populates="categories")
